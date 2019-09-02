810e2300650dbf9b88a4d7ff176662b61b9e88ff
Timothy Dao <timster78811@gmail.com>
2019-09-01 21:43:24 -0400

    starting project

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -42,7 +42,7 @@ export class App extends React.Component {
               
              
               <Route exact path="/login" component={LoginPage} />
-              
+              <Route exact path="/register" component={RegistrationPage} />
           </div>
       ); 
   } 
